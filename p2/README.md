a6fd209bef9e8adc7d6a0f7b462d2ffdf744fb65
Rachel McElwain <rachelhamadi@g.harvard.edu>
2023-10-25 21:09:05 +0000

    commit

---
+++ p2/README.md
@@ -0,0 +1,9 @@
+# Project 2
++ By: Rachel McElwain
++ URL: <http://e2p2.rachelmhes.me>
+
+## Outside resources
+n/a
+
+## Notes for instructor
+n/a
---
+++ p2/index-view.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+
+    <title>Project 2 - Rock, Paper, Scissors</title>
+    <meta charset='utf-8'>
+    <link href=data: , rel=icon>
+
+</head>
+
+<body>
+
+    <h1>Project 2 - Rock, Paper, Scissors</h1>
+</body>
+
+
+</html>

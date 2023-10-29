07993c228db960bc6c8223afa17cf11af9e4bee7
Rachel McElwain <rachelhamadi@g.harvard.edu>
2023-10-29 20:49:05 +0000

    commit

--- p2/index-view.php
+++ p2/index-view.php
@@ -2,7 +2,7 @@
 <html lang='en'>
 
 <head> 
-    <title></title>
+    <title>Project 2 - Rock, Paper, Scissors</title>
     <meta charset='utf-8'>
     <link href=data:, rel=icon>
 
@@ -10,7 +10,18 @@
 
 <body>
     <h2>Project 2</h2>
+
+    <form method='POST' action='process.php'>
+
+        <input type='radio' id='rock' name='pick' value='rock'><label for='rock'>Rock</label>
+        <input type='radio' id='paper' name='pick' value='paper'><label for='paper'>Paper</label>
+        <input type='radio' id='scissors' name='pick' value='scissors'><label for='scissors'>Scissors</label>
+
+        <button type='submit'>LET'S GO!</button>
+    </form>
+
     <h2>Game Mechanics</h2>
+
     <h2>Results</h2>
         <ul>
             <li>Player A chose <?php echo $playerA ?></li>

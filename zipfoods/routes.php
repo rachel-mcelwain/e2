d487ad90f7d500b23815c61b5fed4de03a012813
Rachel McElwain <rachelhamadi@g.harvard.edu>
2023-11-15 16:48:07 +0000

    commit

---
+++ zipfoods/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+# Define the routes of your application
+
+return [
+    # Ex: The path `/` will trigger the `index` method within the `AppController`
+    '/' => ['AppController', 'index'],
+    '/contact' => ['AppController', 'contact'],
+    '/about' => ['AppController', 'about']
+];
---
+++ zipfoods/views/about.blade.php
@@ -0,0 +1,3 @@
+<h1>About</h1>
+
+<p>Zipfoods is your one-stop shop</p>

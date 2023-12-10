f8efaa54263794388cecebaaecbe4937918d0029
Rachel McElwain <rachelhamadi@g.harvard.edu>
2023-12-10 18:45:15 +0000

    commit

---
+++ p3/index-view.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang='en'>
+
+<head> 
+    <title>Project 3 - Rock, Paper, Scissors</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+
+</head>
+
+<body>
+
+    <h2>Project 2</h2>
+
+    <h2>Game Mechanics</h2>
+        <ul>
+            <li>You will be player A</li>
+            <li>Please choose either Rock, Paper, or Scissors as your element</li>
+            <li>Once you have chosen, press the "LET'S GO" button to submit</li>
+            <li>Remember, Rock beats scissors, Scissors beat Paper, and Paper beats Rock!</li>
+        </ul>
+
+</body>
+
+</html>
